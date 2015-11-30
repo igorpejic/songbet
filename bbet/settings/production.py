a7d73c8d5d3df04d27e43f3f5f0d58d87eccb9ec
Igor Pejic <igor.pejic@dr.com>
2015-11-30 12:57:13 +0100

    Production settings.

--- bbet/settings/production.py
+++ bbet/settings/production.py
@@ -1,2 +1,13 @@
 # for now fetch the development settings only
 from dev import *
+
+ALLOWED_HOSTS = ['songbet.me', 'localhost']
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+
+ADMINS = (
+    ('Igor Pejic', 'igorpejicw@gmail.com'),
+)
+
+MANAGERS = ADMINS
