3bde3711ffbe106d1fa41c35eae81668b6d220fd
Igor Pejic <igor.pejic@dr.com>
2015-12-08 23:30:13 +0100

    Login better UX.

--- static/src/app/auth/controllers/login.js
+++ static/src/app/auth/controllers/login.js
@@ -1,5 +1,5 @@
 angular.module('app.auth')
-  .controller('LoginCtrl', function($scope, $rootScope, $alert, $auth, $state, $resource) {
+  .controller('LoginCtrl', function($scope, $rootScope, $alert, $auth, $state, $resource, Notification) {
     var socialUser = $resource('/api/socialuser/', null, {'query': {method: 'GET', isArray:false}});
     $scope.login = function() {
       $auth.login({ username: $scope.email, password: $scope.password })
@@ -18,14 +18,12 @@ angular.module('app.auth')
             );
         })
         .catch(function(response) {
-          $alert({
-            content: response.data.message,
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        });
+            if (response.status === 400) {
+                Notification.error("Incorrect password.");
+            }}
+            );
     };
+
     $scope.authenticate = function(provider) {
         $auth.authenticate(provider)
             .then(function() {
